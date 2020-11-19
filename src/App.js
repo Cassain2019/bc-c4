fe09c172c1c378a7b66c81694ab4de313f52def1
cassain2019 <cashif.hussain@gmail.com>
2020-11-19 16:50:22 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [repos, setRepos] = useState([{}]);
+  useEffect(() => {
+    async function getRepos(){
+      const response = await fetch ("https://api.github.com/users/cassain2019/repos")
+      const data = await response.json();
+      console.log(data);
+      setRepos(data)
+  }
+    getRepos();
+    
+    
+    
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World from all of us !
+      <ul>
+        {repos.map((repoObj,ind)=>{
+          return(<li key={ind}>{repoObj.name}</li>)
+        })}
+      </ul>
     </div>
   );
 }
